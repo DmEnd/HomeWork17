66181982ade22ce1ee47d0968a347f2896cb9030
DmEnd <Dima_polit-sov@mail.ru>
2022-11-16 00:21:07 +0300

    Добавьте файлы проекта.

---
+++ App1/Account.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    internal class Account<T>
+    {
+        string name;
+        string middleName;
+        string lastName;
+        T accNum;
+        double amount;
+
+        public void SetName(string name, string middleName, string lastName)
+        {
+            this.name = name;
+            this.middleName = middleName;
+            this.lastName = lastName;
+        }
+        public void SetAccNum(T accNum)
+        {
+            this.accNum = accNum;
+        }
+        public void SetAmount(double amount)
+        {
+
+            this.amount = Math.Round(amount, 2);
+        }
+        public void SetNameConsole()
+        {
+            Console.Write("Введите имя ");
+            name = Convert.ToString(Console.ReadLine());
+            Console.Write("Введите отчество ");
+            middleName = Convert.ToString(Console.ReadLine());
+            Console.Write("Введите фамилию ");
+            lastName = Convert.ToString(Console.ReadLine());
+            Console.WriteLine();
+        }
+        public void SetAccNumConsole()
+        {
+            Console.Write("Введите номер счёта ");
+            accNum = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine();
+        }
+        public void SetAmountConsole()
+        {
+            Console.Write("Введите баланс счёта ");
+            amount = Math.Round(Convert.ToDouble(Console.ReadLine()), 2);
+            Console.WriteLine();
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("{0} {1} {2} номер счёта {3}, баланс {4:N2}.", lastName, name, middleName, accNum, amount);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ App1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account<string> account1 = new Account<string>();
+            Account<int> account2 = new Account<int>();
+
+            account1.SetNameConsole();
+            account1.SetAccNumConsole();
+            account1.SetAmountConsole();
+
+            account2.SetName("Иван", "Иванович", "Иванов");
+            account2.SetAccNum(525);
+            account2.SetAmount(700568.15);
+
+            account1.ShowInfo();
+            account2.ShowInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
